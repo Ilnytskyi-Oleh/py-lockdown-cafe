ef25bdd07f384ca858c5b0def86208dbe3e7ac33
Oleh Ilnytskyi <ilnytskyy67922@gmail.com>
2025-10-27 09:50:55 +0000

    Sol v3

--- app/main.py
+++ app/main.py
@@ -6,18 +6,12 @@ def go_to_cafe(friends: list[dict], cafe: Cafe) -> str:
     if not friends:
         return f"Friends can go to {cafe.name}"
 
+    masks_to_buy = 0
     for friend in friends:
         try:
             cafe.visit_cafe(friend)
         except VaccineError:
             return "All friends should be vaccinated"
-        except NotWearingMaskError:
-            pass
-
-    masks_to_buy = 0
-    for friend in friends:
-        try:
-            cafe.visit_cafe(friend)
         except NotWearingMaskError:
             masks_to_buy += 1
 
